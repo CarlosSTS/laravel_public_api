c2e5064f2bed4238f737044f5dd4eb760604063a
Carlos Ferreira <63306370+CarlosSTS@users.noreply.github.com>
2026-06-13 17:24:38 -0300

    feat: Fallback route

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,9 @@ Route::prefix('v1')->group(function () {
 Route::prefix('v2')->group(function () {
     require base_path('routes/api_v2.php');
 });
+
+
+// Fallback route for unmatched API requests
+Route::fallback(function () {
+    return response()->json(['message' => 'Not Found.'], 404);
+});
