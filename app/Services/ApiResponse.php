1469a6d0747ab1381f138c03fb7f8a23336ee313
Carlos Ferreira <63306370+CarlosSTS@users.noreply.github.com>
2026-06-13 17:38:38 -0300

    feat: DRY success return

---
+++ app/Services/ApiResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+class ApiResponse
+{
+    public static function success($data, $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+}
